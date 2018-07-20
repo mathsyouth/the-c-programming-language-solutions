f84b77e6024fd765cf9f5acf3562a2ed98288d84
mathsyouth <lh.zeng@hotmail.com>
2018-07-20 13:43:40 +0800

    Set the Sublime text as as 'Indent Using Spaces'

--- Chapter1/exercise1-9.c
+++ Chapter1/exercise1-9.c
@@ -2,79 +2,79 @@
 
 int main(void)
 {
-	int c, blanks;
+    int c, blanks;
 
-	blanks = 0; // recognize whether there is a blank after a character
-	while ((c = getchar()) != EOF) {
-		if (c == ' ') {
-			if (blanks == 0) {
-				putchar(c);
-				blanks = 1;
-			}
-		}
+    blanks = 0; // recognize whether there is a blank after a character
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (blanks == 0) {
+                putchar(c);
+                blanks = 1;
+            }
+        }
 
-		if (c != ' ') {
-			putchar(c);
-			blanks = 0;
-		}
-	}
+        if (c != ' ') {
+            putchar(c);
+            blanks = 0;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 
 /*
 int main(void)
 {
-	int current, previous; // current = current character, previous = previous character
+    int current, previous; // current = current character, previous = previous character
 
-	// set previous to a value that wouldn't match any character
-	previous = EOF;
-	while ((current = getchar()) != EOF) {
-		if (current == ' ') {
+    // set previous to a value that wouldn't match any character
+    previous = EOF;
+    while ((current = getchar()) != EOF) {
+        if (current == ' ') {
             if (previous != ' ')
                 putchar(current);
-		}
-		if (current != ' ')
-			putchar(current);
-		previous = current;
-	}
+        }
+        if (current != ' ')
+            putchar(current);
+        previous = current;
+    }
 
-	return 0;
+    return 0;
 }
 */
 
 /*
 int main(void)
 {
-	int current, previous; // current = current character, previous = previous character
+    int current, previous; // current = current character, previous = previous character
 
-	// set previous to a value that wouldn't match any character
-	previous = EOF;
-	while ((current = getchar()) != EOF) {
-		if (!((current == ' ') && (previous == ' ')))
-			putchar(current);
-		previous = current;
-	}
+    // set previous to a value that wouldn't match any character
+    previous = EOF;
+    while ((current = getchar()) != EOF) {
+        if (!((current == ' ') && (previous == ' ')))
+            putchar(current);
+        previous = current;
+    }
 
-	return 0;
+    return 0;
 }
 */
 
 /*
 int main(void)
 {
-	int current;
+    int current;
 
-	while ((current = getchar()) != EOF) {
-		if (current == ' ') {
-			while ((current = getchar()) == ' ')
-				;
-			putchar(' ');
-		}
-		putchar(current);
-	}
+    while ((current = getchar()) != EOF) {
+        if (current == ' ') {
+            while ((current = getchar()) == ' ')
+                ;
+            putchar(' ');
+        }
+        putchar(current);
+    }
 
-	return 0;
+    return 0;
 }
 */
